a6d34e51110cf562f561c7034732c48ed5e593ca
ThePotatoPowers <44983465+ThePotatoPowers@users.noreply.github.com>
2020-12-27 00:35:01 -0500

    Add files via upload

---
+++ Hi.java
@@ -0,0 +1,7 @@
+public class Hi { //yo yo yo its me thepotatopowers
+    public static void main(String[] args) {
+        int bruh;
+        bruh = 2;
+        System.out.println(bruh);
+    }
+  } 
