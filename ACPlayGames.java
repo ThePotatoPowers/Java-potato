91f7d0c9122407489f7ed50801f05bb321ecbeb1
ThePotatoPowers <44983465+ThePotatoPowers@users.noreply.github.com>
2020-12-28 11:28:19 -0500

    Add files via upload

---
+++ ACPlayGames.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; //import input utility
+public class ACPlayGames {
+    public static void main(String...args) {
+    int a = 1;
+        while (a == 1) {
+        Scanner x = new Scanner(System.in);
+        String y;
+        y =  x.nextLine();
+        int z = Integer.valueOf(y);
+        System.out.println("Yo what's up, I'm turning".concat(" ") + z + (" ").concat("years old today"));
+
+        } 
+        
+        
+    }
+    
+}
