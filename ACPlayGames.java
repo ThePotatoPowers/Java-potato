31a98936bc0bc7532141c005b1ec3c6a91aff1a2
ThePotatoPowers <44983465+ThePotatoPowers@users.noreply.github.com>
2020-12-28 11:34:16 -0500

    Update ACPlayGames.java

--- ACPlayGames.java
+++ ACPlayGames.java
@@ -3,6 +3,7 @@ public class ACPlayGames {
     public static void main(String...args) {
     int a = 1;
         while (a == 1) {
+        System.out.println("Happy birthday! How old are you turning today?");
         Scanner x = new Scanner(System.in);
         String y;
         y =  x.nextLine();
