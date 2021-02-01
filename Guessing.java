955d134c8d650b83b37d14a9469e5db6c06beddf
ThePotatoPowers <44983465+ThePotatoPowers@users.noreply.github.com>
2021-02-01 16:45:21 -0500

    Update Guessing.java

--- Guessing.java
+++ Guessing.java
@@ -1,57 +1,63 @@
 import java.util.*;
-
-public class Guessing {
-    static final Scanner scan = new Scanner(System.in);
+public class Randomtest {
 
     static int entire = 1; //entire is the variable used to restart the ENTIRE loop
     static int incorrect = 1; //incorrect is the variable used to restart the loop if your guess is wrong
     static int Scoreboard = 0; //to measure score
+    static Scanner input = new Scanner(System.in); //input used for entire code
 
     public static void main(String[] args) {
-        while (entire == 1) { 
-            incorrect = 1;
-            System.out.println("Welcome to the number guessing game! Just a second, we're picking a number between 0-20! \nInput your guess");
+    System.out.println("Welcome to the number guessing game! Let's play!");
+    
+    while (entire == 1) { 
+        System.out.println("Input your guess");
+        int number = (int)(Math.random() * 20);  // 0 to 20
+        incorrect = 1;
 
-            int number = (int)(Math.random() * 21);  // 0 to 20
+        while (incorrect == 1) {
+            String guess = input.nextLine();
+            int test = Integer.parseInt(guess);
 
-            while (incorrect == 1) {
-                try {
-                    String guess = scan.nextLine();
-                    int bruhguess = Integer.parseInt(guess);
-                    if (bruhguess == number) {
-                        System.out.println("Wow, you got it! Good job!");
-                        restart();
-                    }
-                    else {
-                        System.out.println("Sorry! Keep trying!"); 
-                    }
-                }
-                catch(Exception e) {
-                    System.out.println("Not a number, please try again");
-                }
-                finally {
-                    incorrect = 1;
-                }
+            if (test == number) {
+            System.out.println("Wow, you got it!");
+            restart();
             }
-        }
-    }
 
-    static void restart() {
-        System.out.println("Would you like to play again? Yes or No");
+            else {
+            System.out.println("Not quite! Keep going though");
+            }
 
-        String answer = scan.nextLine();
+    
+            } //ends incorrect loop
 
-        if (answer.toLowerCase().equals("yes")) {
-            System.out.println("OK!");
-            ++Scoreboard;
-            System.out.println("Your score:" + Scoreboard);
-        }
-        else {
-            System.out.println("Goodbye!");
-            ++Scoreboard;
-            System.out.println("Your score:" + Scoreboard);
-            entire = 2;
-        }
-        incorrect = 2;
-    }
-}
+        }//ends entire  loop 
+        
+    
+    }//ends main  
+
+static void restart() {
+    System.out.println("Would you like to play again? Yes or No");
+
+    String answer = input.nextLine();
+
+    if (answer.toLowerCase().equals("yes")) {
+        System.out.println("OK!");
+        ++Scoreboard;
+        System.out.println("Your score:" + Scoreboard);    
+    }//ends if statement
+    else {
+        System.out.println("Goodbye!");
+        ++Scoreboard;
+        System.out.println("Your score:" + Scoreboard);
+        entire = 2;
+        }//ends else statment
+    incorrect = 2;
+    } //ends restart function
+    
+
+} //ends all code
+
+
+
+
+
